781c6865fbb8be2aec1279cde35510229ba2e91c
BrendonPrado <brendonmatheus123@gmail.com>
2019-02-28 18:29:05 -0300

    teste dominio categoria

--- src/main/java/br/com/gsw/springBootCurso/SpringBootCurso/resource/CategoriaResource.java
+++ src/main/java/br/com/gsw/springBootCurso/SpringBootCurso/resource/CategoriaResource.java
@@ -1,16 +1,28 @@
 package br.com.gsw.springBootCurso.SpringBootCurso.resource;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gsw.springBootCurso.SpringBootCurso.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "rest ok";
+    public List<Categoria> listar(){
+    	
+    	Categoria categoria = new Categoria(1,"informatica");
+    	Categoria categoria2 = new Categoria(2,"Aventura");
+    	List<Categoria> lista = new ArrayList<>();
+    	lista.add(categoria);
+    	lista.add(categoria2);
+    	
+        return lista;
     }
 }
