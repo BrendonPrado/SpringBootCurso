b710c56325b67a78756d1b742347aa6931c03a9c
BrendonPrado <brendonmatheus123@gmail.com>
2019-03-08 17:32:17 -0300

    Feito validação de emails duplicados no sistema na atualização de cliente por @ClienteUpdate

---
+++ src/main/java/br/com/gsw/springBootCurso/SpringBootCurso/service/validation/ClienteUpdateValidator.java
@@ -0,0 +1,6 @@
+package br.com.gsw.springBootCurso.SpringBootCurso.service.validation;
+
+import javax.validation.ConstraintValidator;
+
+public class ClienteUpdateValidator implements ConstraintValidator {
+}
